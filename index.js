1b5ce5fdb0f9d47520085c3e3ec62a6744a0cd6b
javid shabin <javid.main.act@gmail.com>
2024-08-09 02:00:07 +0530

    create env file and add mongodb connection string in env and configue. And connect backend with dataBase

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const express = require('express');
+require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors')
 const PORT = 4000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+// Connect backend with database
+async function main() {
+  await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+}
+main().then(() => console.log("Connected")).catch(err => console.log(err))
 
 
 // Create server
 app.listen(PORT, () => {
-    console.log(`Server running in port: ${PORT}`)
-})
+  console.log(`Server running in port: ${PORT}`);
+});
