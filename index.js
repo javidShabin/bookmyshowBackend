269a37013018a7c525a8e40c0f9f0d256aa5bb07
javid shabin <javid.main.act@gmail.com>
2024-08-09 01:54:20 +0530

    Add two middlewares cors and express

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const PORT = 4000;
+
+app.use(cors())
+app.use(express.json())
+
 
 // Create server
 app.listen(PORT, () => {
