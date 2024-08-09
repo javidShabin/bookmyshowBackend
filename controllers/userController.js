101fc9619e387c4c937eb59611bb16d90c1a1b71
javid shabin <javid.main.act@gmail.com>
2024-08-10 00:57:41 +0530

    Create function in user model

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require('../models/userModel')
+
+const getAllUser = async (req, res) => {
+    res.send("redy")
+}
+
+const addUser = async (req, res) => {
+    res.send("redy")
+}
+
+module.exports = {
+    getAllUser,
+    addUser
+}
