087df13455b538d4c6755b8f302688a8c62c61a8
javid shabin <javid.main.act@gmail.com>
2024-08-09 12:21:23 +0530

    Add fuunction for get all movies, movie by id, and add movie

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,16 +1,21 @@
 const Movie = require('../models/movieModel')
 
 // Get all movies
-const getAllMovies = (req, res) => {
-    res.send("get all movies")
+const getAllMovies = async (req, res) => {
+    const movie = await Movie.find({})
+    res.json(movie)
 }
 // Get movie by Id
-const getMovieById = (req, res) => {
-    res.send("get movie")
+const getMovieById = async (req, res) => {
+    const movie = await Movie.findById(req.params.movieId).exec();
+    res.json(movie)
 }
 // Add movie
-const addMovie = (req, res) => {
-    res.send("add movie")
+const addMovie = async (req, res) => {
+    const movieData = req.body
+    const movie = new Movie(movieData)
+    await movie.save()
+    res.json(movie)
 }
 // Update movie
 const updateMovie = (req, res) => {
