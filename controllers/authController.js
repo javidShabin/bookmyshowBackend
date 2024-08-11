bb9a4ed173d05d32d7fdfae75faf59febe128e60
javid shabin <javid.main.act@gmail.com>
2024-08-11 20:43:20 +0530

    Add some security add rexprire time and the token dont get with js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,35 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
-    const {email, password} = req.body;
-    // find user using email
-    const user = await User.findOne({email: email})
-    // if user is not fond send error as response
-    if (!user) {
-        return res.status(401).send("Unauthorised status")
-    }
-    // if have user existes check password
-    const passwordsMatch = bcrypt.compareSync(password, user.password);
-    
-    if (passwordsMatch) {
-        const token = jwt.sign({_id: user._id, email: user.email}, process.env.SECRET_KEY);
-        res.json(token)
-    }else{
-        res.status(401).send("Unauthorised accsess!")
-    }
-}
+  const { email, password } = req.body;
+  // find user using email
+  const user = await User.findOne({ email: email });
+  // if user is not fond send error as response
+  if (!user) {
+    return res.status(401).send("Unauthorised status");
+  }
+  // if have user existes check password
+  const passwordsMatch = bcrypt.compareSync(password, user.password);
+
+  if (passwordsMatch) {
+    const token = jwt.sign(
+      { _id: user._id, email: user.email },
+      process.env.SECRET_KEY
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.ENVIRONMENT === "Development" ? false : true,
+      maxAge: 1 * 60 * 60 * 100,
+    });
+    res.send("Logged in..!");
+  } else {
+    res.status(401).send("Unauthorised accsess!");
+  }
+};
 
 module.exports = {
-    login
-}
+  login,
+};
