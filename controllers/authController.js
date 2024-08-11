f63a3cee021e55181672692fc8ecc8ba2790170c
javid shabin <javid.main.act@gmail.com>
2024-08-11 12:10:28 +0530

    Ckeck have any user function added and ckeck password using bcrypt. have any user send logged in mssage elase unauthorised accessec

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,22 @@
+const bcrypt = require('bcrypt')
+const User = require("../models/userModel");
 
-const login = (req, res) => {
-
+const login = async (req, res) => {
+    const {email, password} = req.body;
+    // find user using email
+    const user = await User.findOne({email: email})
+    // if user is not fond send error as response
+    if (!user) {
+        return res.status(401).send("Unauthorised status")
+    }
+    // if have user existes check password
+    const passwordsMatch = bcrypt.compareSync(password, user.password);
+    
+    if (passwordsMatch) {
+        res.send("Logged in")
+    }else{
+        res.status(401).send("Unauthorised accsess!")
+    }
 }
 
 module.exports = {
